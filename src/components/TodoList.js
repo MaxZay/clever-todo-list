775456ac8577a1f3264688141390497b5ba29ce6
MaxZay <maximzay2017@gmail.com>
2021-11-15 23:24:07 +0300

    feat: add todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,46 +1,20 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import TodoTask from './TodoTask'
-import { collection, getDocs } from 'firebase/firestore'
-import { db } from '../utils/firebase'
 import { DateContext } from './Main'
-import { Context } from '../App'
+import '../styles/todoList.css'
+import {TaskContext} from "./ToDo";
 
 const TodoList = () => {
-  const [tasks, setTasks] = useState([])
-
-  const { user } = useContext(Context)
-
-  const [currentUser] = user
+  const {tasks, setTasks} = useContext(TaskContext)
 
   const { selectedDate } = useContext(DateContext)
 
-  const tasksCollectionRef = collection(db, 'todo')
-
-  useEffect(() => {
-    const getTasks = async () => {
-      const data = await getDocs(tasksCollectionRef)
-      let result = []
-      data.docs.forEach((item) => {
-        console.log(item.data(), selectedDate, currentUser)
-        if (
-          item.data().date === selectedDate.date &&
-          item.data().day === selectedDate.day &&
-          item.data().year === selectedDate.year &&
-          item.data().userId === currentUser.id
-        ) {
-          result.push({ ...item.data(), id: item.id })
-        }
-      })
-      setTasks([...result])
-    }
-    getTasks()
-  }, [selectedDate])
-
-  console.log(tasks)
   return (
-    <div>
+    <div className={'todo-list__wrapper'}>
       {tasks &&
-        tasks.map((item) => <TodoTask key={item.id} task={item.task} />)}
+        tasks.filter((item) => item.day === selectedDate.day
+          && item.date === selectedDate.date
+          && item.year === selectedDate.year).map((item) => <TodoTask key={item.id} task={item.task} />)}
     </div>
   )
 }
--- src/components/TodoTask.js
+++ src/components/TodoTask.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import '../styles/todoTask.css'
 
 const TodoTask = ({ task }) => {
-  return <div>{task}</div>
+  const [change, setChange] = useState(false)
+
+  const [currentTask, setCurrentTask] = useState(task)
+
+  const clickHandler = () => {
+    setChange(change === true ? false : true)
+  }
+
+  const changeHandler = (event) => {
+    setCurrentTask(event.target.value)
+  }
+
+  return <div className={'todo-task__wrapper'}>
+    <input className={'todo-task__checkbox'} type={'checkbox'}/>
+    <input className={'todo-task__text'} onChange={changeHandler} value={currentTask} disabled={change} />
+  </div>
 }
 
 export default TodoTask
